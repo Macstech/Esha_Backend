3bd15391e774f21ec996298250b11ed3174a070f
Bhuvanesh <bhuvanmca88@gmail.com>
2026-06-13 14:25:15 +0530

    Esha backend update

--- src/routes/loads.js
+++ src/routes/loads.js
@@ -6,7 +6,7 @@ const { authenticate, authorize } = require("../middleware/auth");
 router.get("/", authenticate, ctrl.list);
 router.get("/:id", authenticate, ctrl.getOne);
 router.post("/", authenticate, authorize("SUPER_ADMIN", "EDITOR"), ctrl.create);
-router.patch("/:id", authenticate, authorize("SUPER_ADMIN", "EDITOR"), ctrl.update);
+router.patch("/:id", authenticate, authorize("SUPER_ADMIN", "EDITOR", "VENDOR"), ctrl.update);
 router.delete("/:id", authenticate, authorize("SUPER_ADMIN"), ctrl.remove);
 
 module.exports = router;
